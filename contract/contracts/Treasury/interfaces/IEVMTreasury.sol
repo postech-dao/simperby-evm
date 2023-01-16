030faeb093939bf4b6737434b5c89b8699de5bcb
Lewis <77823566+hyeonLewis@users.noreply.github.com>
2023-01-16 15:51:16 +0900

    Fix naming conventions

--- contract/contracts/Treasury/interfaces/IEVMTreasury.sol
+++ contract/contracts/Treasury/interfaces/IEVMTreasury.sol
@@ -9,40 +9,40 @@ interface IEVMTreasury {
     }
 
     struct FungibleTokenTransfer {
-        address token_address;
+        address tokenAddress;
         uint256 amount;
-        address receiver_address;
-        uint256 contract_sequence;
+        address receiverAddress;
+        uint256 contractSequence;
     }
 
     struct NonFungibleTokenTransfer {
-        address collection_address;
-        uint256 token_index;
-        address receiver_address;
-        uint256 contract_sequence;
+        address collectionAddress;
+        uint256 tokenIndex;
+        address receiverAddress;
+        uint256 contractSequence;
     }
 
     struct Custom {
         string message;
-        uint256 contract_sequence;
+        uint256 contractSequence;
     }
 
     struct Client {
         uint256 height;
-        bytes last_header;
-        string chain_name;
+        bytes lastHeader;
+        string chainName;
     }
 
-    function transfer_token(
+    function transferToken(
         DeliverableMessage _message,
         bytes memory _data,
         uint256 height,
         string memory merkleProof
     ) external;
 
-    function update_light_client(bytes memory header, bytes[] memory proof) external;
+    function updateLightClient(bytes memory header, bytes[] memory proof) external;
 
-    function verify_transaction_commitment(
+    function verifyTransactionCommitment(
         DeliverableMessage message,
         uint256 height,
         string memory MerkleProof
